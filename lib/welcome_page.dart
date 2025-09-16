8ba9d8b3f28d1438eccc854b6540ac44a439e548
Matheus Olegario <mgolegario.br@gmail.com>
2025-09-15 23:22:36 -0300

    testing welcome page

--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -21,7 +21,7 @@ class WelcomePage extends StatelessWidget {
             ),
             const SizedBox(height: 20),
             const Text(
-              "Manage your finances with Econance",
+              "Manage your finances with us",
               style: TextStyle(fontSize: 18, color: Colors.white70),
               textAlign: TextAlign.center,
             ),
