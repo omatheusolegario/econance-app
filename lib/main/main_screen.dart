c28708e7ce8120ae14a08dfe5616e242ecb812e6
Luiz Felipe <127757663+Luiz07102007@users.noreply.github.com>
2025-09-30 00:23:35 -0300

    Starting login with phone, config and small fixes

--- lib/main/main_screen.dart
+++ lib/main/main_screen.dart
@@ -23,7 +23,6 @@ class _MainScreenState extends State<MainScreen> {
     const RevenuesExpensesPage(),
     const AddCategoryPage(),
     const CategoriesPage(),
-    const Config(),
   ];
 
   void _onTabTap(int index) {
